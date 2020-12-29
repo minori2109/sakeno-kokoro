eb5d089a6db0b3ef5dd85ae863f9820bb71a3ac9
minori2109 <minori.ss0921@gmail.com>
2020-12-29 22:41:43 +0900

    商品情報登録ページの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,15 @@ class Item < ApplicationRecord
   belongs_to :rice
   belongs_to :storage_method
   belongs_to :kuramoto
+
+  with_options presence: { message: 'を入力してください' } do
+    validates :name
+    validates :info
+    validates :size
+    validates :alcohol_degree
+    validates :prefecture_id
+    validates :delivery_method
+    validates :price
+  end
+
 end
