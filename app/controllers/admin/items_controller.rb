a5141bcdee5a6d52ad2fd7bc1f292b8f4bba5424
minori2109 <minori.ss0921@gmail.com>
2020-12-29 19:17:23 +0900

    管理者権限の追加

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,26 @@
+class Admin::ItemsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :if_not_admin
+
+  def index
+    @items = Item.all.page(params[:page]).per(10)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    render action: "new" unless @item.save
+  end
+
+  private
+  def if_not_admin
+    redirect_to root_path unless current_user.admin?
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :price, :info, :size, :alcohol_degree, :category_id, :rice_id, :storage_method_id, :kuramoto_id, :prefecture_id, :delivery_method)
+  end
+end
