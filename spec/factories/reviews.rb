edcf2b04c8c3cd7fdbe5a048702de2b9bed382ad
minori2109 <minori.ss0921@gmail.com>
2021-01-07 16:52:13 +0900

    Reviewモデルの単体テストコード実装

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :review do
-    
+    taste            { Faker::Number.within(range: -5..5) }
+    scent            { Faker::Number.within(range: -5..5) }
+    recommend_score  { Faker::Number.within(range: 1..3) }
+    otsumami         { Faker::Lorem.word }
+    comment          { Faker::Lorem.paragraph }
+    association :user
+    association :item
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,119 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @review = FactoryBot.build(:review)
+  end
+
+  describe '商品レビュー投稿機能' do
+    context '商品レビューが投稿できるとき' do
+      it '必須事項が記入されていれば保存できる' do
+        expect(@review).to be_valid
+      end
+
+      it '評価が小数点の点数でも保存できる' do
+        @review.recommend_score = 3.5
+        expect(@review).to be_valid
+      end
+
+      it '味の評価がマイナスでも保存できる' do
+        @review.taste = -1
+        expect(@review).to be_valid
+      end
+
+      it '香りの評価がマイナスでも保存できる' do
+        @review.scent = -5
+        expect(@review).to be_valid
+      end
+
+      it 'おすすめのおつまみが空でも保存できる' do
+        @review.otsumami = nil
+        expect(@review).to be_valid
+      end
+
+      it 'おすすめのおつまみが200文字で保存できる' do
+        @review.otsumami = Faker::Lorem.characters(number: 200)
+        expect(@review).to be_valid
+      end
+
+      it 'レビュー内容が1000文字で保存できる' do
+        @review.comment = Faker::Lorem.characters(number: 1000)
+        expect(@review).to be_valid
+      end
+    end
+
+    context '商品レビューが投稿できないとき' do
+      it '評価が空では保存できない' do
+        @review.recommend_score = nil
+        @review.valid?
+        expect(@review.errors[:recommend_score]).to include('を入力してください')
+      end
+
+      it '評価が0以下では保存できない' do
+        @review.recommend_score = 0
+        @review.valid?
+        expect(@review.errors[:recommend_score]).to include('は1~5の範囲で選択してください')
+      end
+
+      it '評価が6以上では保存できない' do
+        @review.recommend_score = 6
+        @review.valid?
+        expect(@review.errors[:recommend_score]).to include('は1~5の範囲で選択してください')
+      end
+
+      it '味が空では保存できない' do
+        @review.taste = nil
+        @review.valid?
+        expect(@review.errors[:taste]).to include('を入力してください')
+      end
+
+      it '味が-6以下では保存できない' do
+        @review.taste = -6
+        @review.valid?
+        expect(@review.errors[:taste]).to include('は-5~5の範囲で選択してください')
+      end
+
+      it '味が6以上では保存できない' do
+        @review.taste = 6
+        @review.valid?
+        expect(@review.errors[:taste]).to include('は-5~5の範囲で選択してください')
+      end
+
+      it '香りが空では保存できない' do
+        @review.scent = nil
+        @review.valid?
+        expect(@review.errors[:scent]).to include('を入力してください')
+      end
+
+      it '香りが-6以下では保存できない' do
+        @review.scent = -6
+        @review.valid?
+        expect(@review.errors[:scent]).to include('は-5~5の範囲で選択してください')
+      end
+
+      it '香りが6以上では保存できない' do
+        @review.scent = 6
+        @review.valid?
+        expect(@review.errors[:scent]).to include('は-5~5の範囲で選択してください')
+      end
+
+      it 'おすすめのおつまみが201文字以上では保存できない' do
+        @review.otsumami = Faker::Lorem.characters(number: 201)
+        @review.valid?
+        expect(@review.errors[:otsumami]).to include('は200文字以内で入力してください')
+      end
+
+      it 'レビュー内容が空では保存できない' do
+        @review.comment = nil
+        @review.valid?
+        expect(@review.errors[:comment]).to include('を入力してください')
+      end
+
+      it 'レビュー内容が1001文字以上では保存できない' do
+        @review.comment = Faker::Lorem.characters(number: 1001)
+        @review.valid?
+        expect(@review.errors[:comment]).to include('は1000文字以内で入力してください')
+      end
+    end
+  end
 end
