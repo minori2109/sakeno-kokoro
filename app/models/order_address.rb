34302d2b9acdf7ca552dfb9091a24c0a613232a3
minori2109 <minori.ss0921@gmail.com>
2021-01-07 23:12:59 +0900

    Formオブジェクトの作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,19 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'はハイフンを含む半角数字7桁で入力してください' }
+    validates :prefecture_id
+    validates :city
+    validates :block
+    validates :phone_number, numericality: { only_integer: true, message: 'は半角数字で入力してください' }, length: { maximum: 11 }
+    validates :user_id
+    validates :item_id
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
