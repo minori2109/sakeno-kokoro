1c72f88ab86b138db993550c3e8eabcf5feafe0b
minori2109 <minori.ss0921@gmail.com>
2021-02-01 21:17:55 +0900

    ルーティングに追記

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,15 @@ Rails.application.routes.draw do
   resources :items, only: [:index, :show] do
     resources :reviews, only: [:index, :new, :create, :edit, :update, :destroy]
     resources :orders, only: [:index, :create]
+    resources :favorites, only: [:create, :destroy]
   end
   namespace :admin do
     resources :items
   end
 
   resources :reviews, only: [:show]
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    get :favorites, on: :collection
+  end
 
 end
